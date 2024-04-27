e6c48a8438ee3012452d958f903fa4b5576c43c0
peteryuan <ypf9319@gmail.com>
2024-04-27 15:07:22 +0800

    quote

---
+++ src/pages/Quote.js
@@ -0,0 +1,10 @@
+import "./Quote.css";
+
+export default function Quote() {
+    return (
+        <div className="quote">
+            <div className="displayPane"></div>
+            <div className="operatePane"></div>
+        </div>
+    );
+}
--- src/pages/Tools.js
+++ src/pages/Tools.js
@@ -1,5 +1,15 @@
 import "./Tools.css";
+import { NavLink } from "react-router-dom";
+const tools = [{ path: "/quote", name: "Quote" }];
 
 export default function Tools() {
-    return <div className="toolsContainer">Tools</div>;
+    return (
+        <div className="toolsContainer">
+            {tools.map((item, idx) => (
+                <NavLink key={idx} to={item.path}>
+                    {item.name}
+                </NavLink>
+            ))}
+        </div>
+    );
 }
