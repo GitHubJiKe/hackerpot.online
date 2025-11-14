78eafc707589e62d500b95056596d0a1bd64f099
peteryuan <ypf9319@gmail.com>
2025-11-14 12:59:05 +0800

    feature: new article

--- docs/toc.js
+++ docs/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="AI给我带来的疗愈.html"><strong aria-hidden="true">1.</strong> AI 给我带来的疗愈</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="AI给我带来的疗愈.html"><strong aria-hidden="true">1.</strong> AI 给我带来的疗愈</a></li><li class="chapter-item expanded "><a href="在浏览器上阅读电子书.html"><strong aria-hidden="true">2.</strong> 在浏览器上阅读电子书</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
